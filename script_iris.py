fd4e6de814c17bde34560c7fc548ba57603125ae
alessandro romualdi <alessandro@laptop-kr39sn3n.home>
2020-02-23 13:57:58 +0100

    test pca on iris dataset

---
+++ script_iris.py
@@ -0,0 +1,20 @@
+from sklearn import datasets
+from pca import PCA
+
+
+"""
+load data
+"""
+iris = datasets.load_iris()
+
+X = iris.data
+y = iris.target
+target_names = iris.target_names
+
+"""
+pca
+"""
+pca = PCA()
+pca.fit_transform(X)
+plt = pca.plot(y, target_names, title='PCA of IRIS dataset', plot_ellipse=True)
+plt.show()
